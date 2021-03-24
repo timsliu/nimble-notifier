056923204149e0250ae465d584a36aba1a92b67b
timsliu <timliu92@gmail.com>
2021-03-24 15:10:02 -0700

    added exception handling to runner and made empty update_users

--- runner.py
+++ runner.py
@@ -10,7 +10,7 @@ import gmail_utils
 import time
 from datetime import datetime
 
-SLEEP_TIME = 400      # sleep time in seconds
+SLEEP_TIME = 200      # sleep time in seconds
 
 
 def run_test():
@@ -33,17 +33,20 @@ def run_all():
     gmail_service = gmail_utils.GetService()                         # get gmail service
     vaccine_spotter = fetch_data.fetch_vaccine_spotter()             # update vaccine data
     
-    while True: 
-        vaccine_spotter.fetch()                                      # update vaccine data
-        match_list = matcher.match_all()                             # match users w/ nearby vaccines
-        sent, failed = notifier.send_all(match_list, gmail_service)  # send emails
-       
-        now = datetime.now() 
-        print("Sent {} out of {} attempted emails at {}".format(
-            sent, 
-            failed + sent, 
-            now.strftime("%d/%m/%Y %H:%M:%S")
-        ))
+    while True:
+        try:
+            vaccine_spotter.fetch()                                      # update vaccine data
+            match_list = matcher.match_all()                             # match users w/ nearby vaccines
+            sent, failed = notifier.send_all(match_list, gmail_service)  # send emails
+            now = datetime.now() 
+            print("Sent {} out of {} attempted emails at {}".format(
+                sent, 
+                failed + sent, 
+                now.strftime("%d/%m/%Y %H:%M:%S")
+            ))
+        except BaseException as e:
+            print("Failed: {}".format(e))
+        
         time.sleep(SLEEP_TIME)
 
 if __name__ == "__main__":
---
+++ update_users.py
@@ -0,0 +1,15 @@
+# update_users.py
+#
+# This program updates the user JSON using a google spreadsheet.
+
+
+def new_users(drive_service):
+    '''read from the google spreadsheet and find new users'''
+
+
+def add_users(user_list):
+    '''add new users to the appropriate JSON'''
+
+
+def remove_users():
+    pass
